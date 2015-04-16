53f745488ed54c35fdaa4147e8bff213fd6e4778
meredith620 <meredith620@sina.com>
2015-04-16 12:25:30 +0800

    add python

---
+++ utils/python/py3_opt.py
@@ -0,0 +1,20 @@
+#! /bin/env python3
+
+import sys
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-f", "--file",
+                  action="store", dest="filename", type="string", 
+                  help="write report to FILE", metavar="FILE")
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+def main():
+    (options, args) = parser.parse_args()
+    print(options.filename)
+    print(options)
+
+if __name__ == "__main__":
+    main()
+
