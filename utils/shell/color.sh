4a8622dbe4cff130ca72945444e3abe2c767d5f0
meredith620 <meredith620@sina.com>
2015-07-23 22:06:55 +0800

    update config

--- utils/shell/color.sh
+++ utils/shell/color.sh
@@ -3,7 +3,7 @@ T='gYw' # The test text
 echo  
 echo  "        default  40m     41m     42m     43m     44m     45m     46m    47m"  
 ## FGs 为前景(foreground)色, BG 为背景(background)色  
-for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m'  
+for FGs in '    m' '   1m' '  30m' '1;30m' '  31m' '1;31m' '  32m' '1;32m' '  33m' '1;33m' '  34m' '1;34m' '  35m' '1;35m' '  36m' '1;36m' '  37m' '1;37m' '2;31m' '3;31m' '4;31m' '5;31m'
 do  
    FG=$(echo $FGs|tr -d ' ')  
    echo -en " $FGs \033[$FG  $T  "  
