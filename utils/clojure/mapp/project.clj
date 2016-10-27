55ad364aba1a2d49b57f43970af82beb738d1272
meredith <meredith620@sina.com>
2016-10-27 16:38:07 +0800

    lein deploy and fetch problem not solved...

--- utils/clojure/mapp/project.clj
+++ utils/clojure/mapp/project.clj
@@ -3,8 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"local" {:url "file://localhost:9999/home/meredith/workspace/cmlibs" ;http service
-                          :releases {:checksum :ignore}}}
+  :repositories {"local" {; :url "file://home/meredith/workspace/cmlibs"
+                          :url "http://localhost:9999/cmlibs"
+                          }}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [mlib "0.2.0-SNAPSHOT"]]
   :main ^:skip-aot mapp.core
