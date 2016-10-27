9684126ba58a281334eef69c7746d92df3598109
meredith <meredith620@sina.com>
2016-10-27 15:15:52 +0800

    update new local repository method

--- utils/clojure/mapp/project.clj
+++ utils/clojure/mapp/project.clj
@@ -1,10 +1,12 @@
-(defproject usemlib "0.1.0-SNAPSHOT"
+(defproject mapp "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"local" {:url "file://localhost:9999/home/meredith/workspace/cmlibs" ;http service
+                          :releases {:checksum :ignore}}}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [mlib "0.1.0-SNAPSHOT"]]
-  :main ^:skip-aot usemlib.core
+                 [mlib "0.2.0-SNAPSHOT"]]
+  :main ^:skip-aot mapp.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- utils/clojure/mlib/project.clj
+++ utils/clojure/mlib/project.clj
@@ -1,11 +1,11 @@
-(defproject mlib "0.1.0-SNAPSHOT"
+(defproject mlib "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :plugins [[org.apache.maven.wagon/wagon-ssh-external "2.6"]]
-  :repositories [["myrepo" "scp://localhost/home/meredith/workspace/clojure_mlibs"]])
+  :repositories [["local" "scp://localhost/home/meredith/workspace/cmlibs"]])
 
 (cemerick.pomegranate.aether/register-wagon-factory!
  "scp" #(let [c (resolve 'org.apache.maven.wagon.providers.ssh.external.ScpExternalWagon)]
