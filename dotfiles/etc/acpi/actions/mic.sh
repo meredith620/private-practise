0209127d46451f2301b1462e8567025d9ad98fe4
meredith620 <meredith620@gmail.com>
2018-03-01 23:47:47 +0800

    update mute button

---
+++ dotfiles/etc/acpi/actions/mic.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+INPUT_DEVICE="'Capture'"
+if amixer sget $INPUT_DEVICE,0 | grep '\[on\]' ; then
+    amixer sset $INPUT_DEVICE,0 toggle
+#    echo "0 blink" > /proc/acpi/ibm/led
+else
+    amixer sset $INPUT_DEVICE,0 toggle
+#    echo "0 on" > /proc/acpi/ibm/led 
+fi
