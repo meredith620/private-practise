e67901938943c09c6e774ae82e00b8c94573f3ad
lvliang <meredith620@gmail.com>
2020-08-24 12:27:24 +0800

    update backup

--- dotfiles/backup.sh
+++ dotfiles/backup.sh
@@ -1,7 +1,33 @@
 #! /bin/bash
-
+CURFILE=$(basename $0)
+_ERR_HDR_FMT="%.23s"
+_ERR_MSG_FMT="[${_ERR_HDR_FMT}][$USER@$HOSTNAME][${CURFILE}:%s][ %b ]: %b\n"
+log_msg()
+{
+    local lineno=$1
+    local tp=$2
+    local msg="$3"
+    printf "$_ERR_MSG_FMT" $(date +%F.%T.%N) ${lineno} "${tp}" "${msg}" 1>&2
+}
+log_info()
+{
+    local msg="$1"
+    log_msg ${BASH_LINENO[0]} "\033[32mINFO\033[0m" "$msg"
+}
+log_warning()
+{
+    local msg="$1"
+    log_msg ${BASH_LINENO[0]} "\033[33mWARN\033[0m" "$msg"
+}
+log_error()
+{
+    local msg="$1"
+    log_msg ${BASH_LINENO[0]} "\033[31mERROR\033[0m" "$msg"
+}
+#----------------------------------------------------------------------------
 ROOT="$(cd $(dirname $0) && pwd)"
 ROOT=${ROOT%/}
+USER_BACKUP_HOME="__home"
 
 is_dir() {
     local target="$1"
@@ -10,87 +36,101 @@ is_dir() {
     return ${ret}
 }
 
-dobackup() {
-    local src="$1"
-    target_parent=$(dirname ${ROOT}${src})
-    mkdir -p ${target_parent}
+do_backup() {
+    local sudo=$1
+    local src=$2
+    local target_parent=$3
+    CP=cp
+    if [ $sudo == true ]; then
+        CP="sudo cp"
+    fi
     if [ ! -e "$src" ]; then
-        echo "src: $src does not exists, skip"
+        log_info "src: $src does not exists, skip"
         return 0
     fi
     is_dir "$src"
     ret=$?
     if [ $ret -eq 0 ]; then
         # is dir
-        echo "cp -r ${src} ${target_parent}/"
-        cp -r ${src} ${target_parent}/
+        echo "$CP -r ${src} ${target_parent}/"
+        $CP -r ${src} ${target_parent}/
     else
         # is not dir
-        echo "cp ${src} ${target_parent}/"
-        cp ${src} ${target_parent}/
+        echo "$CP ${src} ${target_parent}/"
+        $CP ${src} ${target_parent}/
     fi
 }
 
-BACKUP_LIST=(
-    /etc/X11/xorg.conf.d
-    /etc/acpi/actions
-    /etc/acpi/events
-    /etc/asound.conf
-    /etc/bluetooth
-    /etc/conf.d/net
-    /etc/default/grub
-    /etc/fstab
-    #etc/grub.d/40_custom
-    /etc/ipsec.conf
-    /etc/ipsec.secrets
-    #/etc/modprobe.d/nouveau.conf
-    /etc/portage/make.conf
-    /etc/portage/package.keywords
-    /etc/portage/package.use
-    /etc/portage/repos.conf
-    /etc/portage/package.license
-    /etc/portage/patches
-    /etc/portage/savedconfig
-    /etc/portage/package.accept_keywords
-    /etc/portage/package.mask
-    /etc/portage/repo.postsync.d
-    /etc/profile.d/xdg_cache_home.sh
-    /etc/ssh/ssh_config
-    /etc/ssh/sshd_config
-    /etc/sysctl.conf
-    /etc/sysctl.d/10-net-forward.conf
-    /etc/wpa_supplicant/wpa_supplicant.conf
+system_backup() {
+    local src="$1"
+    target_parent=$(dirname ${ROOT}${src})
+    # echo "src: $src, target_parent: $target_parent"
+    mkdir -p ${target_parent}
+    do_backup true ${src} $t{arget_parent}
+}
 
-    /usr/src/linux/.config
-    ${HOME}/.Xresources
-    ${HOME}/.bashrc
-    ${HOME}/.emacs
-    ${HOME}/.gitconfig
-    # ${HOME}/.inputrc
-    ${HOME}/.mplayer
-    ${HOME}/.sawfish
-    ${HOME}/.sawfishrc
-    ${HOME}/.ssh
-    ${HOME}/.tmux.conf
-    ${HOME}/.toprc
-    ${HOME}/.xbindkeysrc
-    ${HOME}/.xinitrc
-    ${HOME}/.xmodmap
-    ${HOME}/.zshrc
-    ${HOME}/makewallpaper.sh
-    ${HOME}/set_brightness.sh
-    ${HOME}/set_display.sh
-    ${HOME}/wlanup.sh
-)
+user_backup() {
+    local src="$1"
+    local home_src=${src##${HOME}/}
+    home_src=/${home_src%/}
 
-backup_system_conf() {
-    for x in ${BACKUP_LIST[@]}; do
-        dobackup "$x"
+    target_parent=$(dirname ${USER_BACKUP_HOME}${home_src})
+    # echo "src: $src, target_parent: $target_parent"
+    mkdir -p ${target_parent}
+    do_backup false ${src} ${target_parent}
+}
+
+backup_system_and_user() {
+    source $ENVFILE
+    for x in ${SYSTEM_BACKUP_LIST[@]}; do
+        system_backup "$x"
+    done
+    echo "sudo chown $USER:$USER ${ROOT} -R"
+    sudo chown $USER:$USER ${ROOT} -R
+
+    for x in ${USER_BACKUP_LIST[@]}; do
+        user_backup "$x"
     done
 }
 
+usage() {
+    echo "$0 [OPTIONS]
+	-m mode [backup|restore]
+	-e env-file
+	-r range [user|system], user will work in '~', system will work in '/' and with sudo"
+    exit 1
+}
+
+check_parameters() {
+    if [ -z $MODE ] || ([ "$MODE" != "backup" ] && [ "$MODE" != "restore" ]); then
+        log_error "unrecognized parameter MODE: '$MODE'"
+        usage
+    fi
+    if [ -z $ENVFILE ] || [ ! -e $ENVFILE ]; then
+        log_error "file not exists, ENVFILE: ${ENVFILE}"
+        usage
+    fi
+}
+
 main() {
-    backup_system_conf
+    while getopts "m:e:h" opt; do
+        case "$opt" in
+            m)
+                MODE="${OPTARG}"
+                ;;
+            e)
+                ENVFILE="${OPTARG}"
+                ;;
+            h)
+                usage
+                ;;
+            *)
+                usage
+                ;;
+        esac
+    done
+    check_parameters
+    backup_system_and_user
 }
 
 # ===== main =====
