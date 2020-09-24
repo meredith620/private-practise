faeae95cc670a72108399d107dec7176b168d42a
lvliang <meredith620@gmail.com>
2020-09-24 10:33:23 +0800

    update backup

--- dotfiles/etc/acpi/actions/mute.sh
+++ dotfiles/etc/acpi/actions/mute.sh
@@ -5,8 +5,8 @@ if amixer sget $INPUT_DEVICE,0 | grep '\[off\]' ; then
     amixer set 'Master',0 unmute
     amixer set 'Headphone',0 unmute
     amixer set 'Speaker',0 unmute
-#    echo "0 blink" > /proc/acpi/ibm/led
+   # echo "0 blink" > /proc/acpi/ibm/led
 else
     amixer sset $INPUT_DEVICE,0 toggle
-#    echo "0 on" > /proc/acpi/ibm/led 
+   # echo "0 on" > /proc/acpi/ibm/led 
 fi
